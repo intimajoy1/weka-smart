265da3610e2a745b67852641efb6c93933911d51
Alpha7 <henryomondi6612@gmail.com>
2025-08-02 00:02:20 +0300

    Add Front-End

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -8,9 +8,9 @@ function Footer() {
   const {isAuthorized}  = useContext(Context)
   return (
     <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
-<div>&copy; All Rights Reserved by Abhishek.</div>
+<div>&copy; All Rights Reserved by Scenius Solutions.</div>
 <div>
-  <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link>
+  {/* <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link> */}
   <Link to={'https://leetcode.com/u/exclusiveabhi/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
   <Link to={'https://www.linkedin.com/in/abhishek-rajput-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
   <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
